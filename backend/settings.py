d79dca267727d912e2c5dc1182cd231966dcde5c
ahsanumair1199 <ahsanumair1199@gmail.com>
2022-05-21 17:42:59 +0500

    final commit

--- backend/settings.py
+++ backend/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l!c#-o&^q5a%u8t(e#-#h50bkh&o6fhq2h1pajhfr!9rj8#qsf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
